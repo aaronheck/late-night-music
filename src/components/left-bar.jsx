18826f84346e4192e84311a693a293dc27d40edf
aaronheck <aaron.rosenheck@gmail.com>
2017-05-04 20:41:04 -0700

    Scroll to top on date change

--- src/components/left-bar.jsx
+++ src/components/left-bar.jsx
@@ -12,10 +12,10 @@ export default class LeftBar extends Component {
 		<a><img id="top_logo" src={logo_circle} /></a>
 		<div id="left-bar-items">
 			{this.props.dates.map((date, index) => {
-				return <div className={date === this.props.selectedDate ? 'menu-block selected' : 'menu-block'} onClick={() => this.props.handleDateSelect(date)} key={date}>
+				return <a href="#"><div className={date === this.props.selectedDate ? 'menu-block selected' : 'menu-block'} onClick={() => this.props.handleDateSelect(date)} key={date}>
 							<div className="numerical-date">{date}</div>
 							<div className="day-of-week">{daysOfWeek[index]}</div>
-						</div>;
+						</div></a>;
 			}, this)}
 
 			<div className="menu-block about" onClick={()=>window.location.href="/about.html"}>
